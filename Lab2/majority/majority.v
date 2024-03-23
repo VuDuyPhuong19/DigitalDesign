6da3890de340e9134af118260f93cdf357cd575f
VuDuyPhuong19 <146603946+VuDuyPhuong19@users.noreply.github.com>
2024-03-23 19:42:31 +0700

    update

--- Lab2/majority/majority.v
+++ Lab2/majority/majority.v
@@ -3,10 +3,11 @@ module majority #(parameter n = 8)(
 	output y
 );
 
-reg [$clog2(n) - 1:0] i;
+integer i;
 reg [$clog2(n) - 1:0] cnt;
 
 always @ (x) begin
+  cnt  = 0;
 	for(i = 0; i < n; i = i + 1) begin
 		if(x[i]) cnt = cnt + 1;
 	end
@@ -15,3 +16,4 @@ end
 assign y = (cnt > n/2) ? 1'b1 : 1'b0;
 
 endmodule
+
